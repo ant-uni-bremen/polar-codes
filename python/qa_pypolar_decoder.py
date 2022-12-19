05611fea0ce1bef78cba87ba2d1570ee553affdf
Johannes Demel <demel@ant.uni-bremen.de>
2022-12-19 12:49:48 +0100

    ci: Fix more decoder tests
    Signed-off-by: Johannes Demel <demel@ant.uni-bremen.de>

--- python/qa_pypolar_decoder.py
+++ python/qa_pypolar_decoder.py
@@ -8,9 +8,21 @@
 
 import numpy as np
 import unittest
-from polar_code_tools import design_snr_to_bec_eta, calculate_bec_channel_capacities, get_frozenBitMap, get_frozenBitPositions, get_polar_generator_matrix, get_polar_encoder_matrix_systematic, frozen_indices_to_map
+from polar_code_tools import (
+    design_snr_to_bec_eta,
+    calculate_bec_channel_capacities,
+    get_frozenBitMap,
+    get_frozenBitPositions,
+    get_polar_generator_matrix,
+    get_polar_encoder_matrix_systematic,
+    frozen_indices_to_map,
+)
 from polar_code_tools import get_info_indices, get_expanding_matrix, calculate_ga
-from channel_construction import ChannelConstructorBhattacharyyaBounds, ChannelConstructorGaussianApproximationDai, ChannelConstructorBetaExpansion
+from channel_construction import (
+    ChannelConstructorBhattacharyyaBounds,
+    ChannelConstructorGaussianApproximationDai,
+    ChannelConstructorBetaExpansion,
+)
 import pypolar
 
 
@@ -22,14 +34,14 @@ class PolarDecoderTests(unittest.TestCase):
         pass
 
     def test_002_frozen_bit_positions(self):
-        for snr in np.arange(-1.5, 3.5, .25):
+        for snr in np.arange(-1.5, 3.5, 0.25):
             for inv_coderate in np.array([8, 6, 5, 4, 3, 2, 1.5, 1.2]):
                 for n in range(6, 11):
                     N = 2 ** n
                     K = int(N / inv_coderate)
                     # print(N, K, inv_coderate)
                     cf = pypolar.frozen_bits(N, K, snr)
-                    eta = design_snr_to_bec_eta(snr, 1. * K / N)
+                    eta = design_snr_to_bec_eta(snr, 1.0 * K / N)
                     polar_capacities = calculate_bec_channel_capacities(eta, N)
                     pf = get_frozenBitPositions(polar_capacities, N - K)
                     pf = np.sort(pf)
@@ -52,51 +64,55 @@ class PolarDecoderTests(unittest.TestCase):
 
     def initialize_encoder(self, N, K, snr):
         try:
-            np.seterr(invalid='raise')
+            np.seterr(invalid="raise")
             cc = ChannelConstructorBetaExpansion(N, snr)
         except FloatingPointError:
-            np.seterr(invalid='warn')
+            np.seterr(invalid="warn")
             cc = ChannelConstructorBetaExpansion(N, snr)
 
-        f = np.sort(cc.getSortedChannels())[0:N - K]
+        f = np.sort(cc.getSortedChannels())[0 : N - K]
         p = pypolar.PolarEncoder(N, f)
         return p
 
     def test_006_cpp_decoder_impls(self):
-        print('TEST: CPP Decoder')
-        snr = -1.
+        print("TEST: CPP Decoder")
+        snr = -1.0
         test_size = np.arange(7, 11, dtype=int)
         # test_size = np.array([4, 5, 6, 8, 9, 10], dtype=int)
         for i in test_size:
             N = 2 ** i
-            self.validate_decoder(N, int(N * .75), snr)
+            self.validate_decoder(N, int(N * 0.75), snr)
             self.validate_decoder(N, N // 2, snr)
             self.validate_decoder(N, N // 4, snr)
             self.validate_decoder(N, N // 8, snr)
 
     def validate_decoder(self, N, K, snr, crc=None):
-        self.run_decoder(N, K, 1, snr, 'char')
-        self.run_decoder(N, K, 1, snr, 'float')
-        self.run_decoder(N, K, 4, snr, 'float')
-        self.run_decoder(N, K, 8, snr, 'float')
-        self.run_decoder(N, K, 4, snr, 'mixed')
+        self.run_decoder(N, K, 1, snr, "char")
+        self.run_decoder(N, K, 1, snr, "float")
+        self.run_decoder(N, K, 4, snr, "float")
+        self.run_decoder(N, K, 8, snr, "float")
+        self.run_decoder(N, K, 4, snr, "mixed")
         if N < 600 and N // K == 2:
-            self.run_decoder(N, K, 4, snr, 'scan')
+            self.run_decoder(N, K, 4, snr, "scan", 16)
 
-    def run_decoder(self, N, K, L, snr, decType):
-        with self.subTest(N=N, K=K, L=L, snr=snr, decType=decType):
-            print("Decoder CPP test (N={}, K={}, L={}, type='{}') -> {}dB".format(N, K, L, decType, snr))
+    def run_decoder(self, N, K, L, snr, decType, detector_size=8):
+        with self.subTest(
+            N=N, K=K, L=L, snr=snr, decType=decType, detector_size=detector_size
+        ):
+            print(
+                f"Decoder CPP test ({N=:4}, {K=:3}, {L=}, {decType=:5}, CRC{detector_size:2}) -> {snr}dB"
+            )
             p = self.initialize_encoder(N, K, snr)
 
             if not self.check_matrix_domination_contiguity(N, p.frozenBits()):
-                print('invalid code parameters!')
+                print("invalid code parameters!")
                 return
 
             f = p.frozenBits()
             dec0 = pypolar.PolarDecoder(N, L, f, decType)
 
-            p.setErrorDetection(8)
-            dec0.setErrorDetection(8)
+            p.setErrorDetection(detector_size)
+            dec0.setErrorDetection(detector_size)
             self.assertListEqual(f, dec0.frozenBits())
 
             for i in np.arange(10):
@@ -106,13 +122,13 @@ class PolarDecoderTests(unittest.TestCase):
                 # The pythonic method
                 cw_pack = p.encode_vector(d)
                 b = np.unpackbits(cw_pack)
-                llrs = -2. * b + 1.
-                llrs += np.random.uniform(-.2, .2, size=llrs.size)
+                llrs = -2.0 * b + 1.0
+                llrs += np.random.uniform(-0.2, 0.2, size=llrs.size)
                 llrs = llrs.astype(dtype=np.float32)
 
                 dhat0 = dec0.decode_vector(llrs)
                 np.testing.assert_equal(dhat0, d)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main(failfast=False)
